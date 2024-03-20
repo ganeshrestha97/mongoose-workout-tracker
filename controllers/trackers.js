b6f0c97567fffec2282d70976499f4780631ffb6
Ganesh <ganeshrestha97@gmail.com>
2024-03-20 00:10:42 -0700

    fourth push

--- controllers/trackers.js
+++ controllers/trackers.js
@@ -4,28 +4,52 @@ const Tracker = require('../models/tracker')
 
 module.exports = {
     create,
+    newTracker,
+    index,
+    show
+}
+
+function newTracker(req, res) {
+    res.render('trackers/new', { title: 'Add New Tracker', errorMsg: '' })
 }
 
 
 async function create(req, res) {
     try {
-        const data = {
+        const newTracker = new Tracker({
+            steps: req.body.steps,
+            weight: req.body.weight,
+            waterIntake: req.body.waterIntake,
+            caloriesIntake: req.body.caloriesIntake,
+            caloriesBurn: req.body.caloriesBurn,
+            heartRate: req.body.heartRate,
             date: req.body.date
-        }
-        if (req.body.steps) data.steps = req.body.steps
-        if (req.body.weight) data.weight =req.body.weight
-        if (req.body.waterIntake) data.weight =req.body.waterIntake
-        if (req.body.caloriesIntake) data.steps = req.body.caloriesIntake
-        if (req.body.caloriesBurn) data.weight =req.body.caloriesBurn
-        if (req.body.heartRate) data.weight =req.body.heartRate
-        
-        await Tracker.create(data)
+    })
+        await newTracker.save()
         res.redirect('/trackers')
-    } catch (error) {
-        console.error('Error saving data!', error)
-        res.redirect('/')
+    } catch (err) {
+        console.error('Error creating tracker!', err)
+        res.redirect('/trackers/new')
     }
 }
 
 
+async function index(req, res) {
+    try {
+        const trackers = await Tracker.find({})
+        res.render('trackers/index', { title: 'All Trackers', trackers})
+    } catch (error) {
+        console.error('Error listing trackers:', error)
+        res.redirect('/')
+    }
+}
 
+async function show(req, res) {
+    try {
+        const tracker = await Tracker.findById(req.params.id)
+        res.render('trackers/show', { title: 'Tracker Detail', tracker })
+    } catch (error) {
+        console.error('Error showing tracker:', error)
+        res.redirect('/trackers')
+    }
+}
--- routes/trackers.js
+++ routes/trackers.js
@@ -3,8 +3,18 @@ let router = express.Router()
 let trackersCtrl = require('../controllers/trackers')
 
 
+// To display all trackers
+router.get('/', trackersCtrl.index)
 
+// To displaythe form for new tracker
+router.get('/new', trackersCtrl.newTracker)
+
+// To show details of specific tracker
+router.get('/:id', trackersCtrl.show)
+
+// To handle form submission and create a new tracker
 router.post('/', trackersCtrl.create)
 
 module.exports = router
+
 
